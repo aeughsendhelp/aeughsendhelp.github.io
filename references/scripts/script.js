0433e523db1950e0af00c5f38afb415e719a370f
evilpunkin <82129788+aeughsendhelp@users.noreply.github.com>
2024-01-12 11:51:51 -0800

    better dropdown system

--- references/scripts/script.js
+++ references/scripts/script.js
@@ -1,15 +1,14 @@
 var coll = document.getElementsByClassName("collapsiblevertical");
-	var i;
 
-	for(i = 0; i < coll.length; i++) {
-		coll[i].addEventListener("click", function() {
-			this.classList.toggle("active");
-			var content = this.nextElementSibling;
+for(var i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function() {
+		var content = this.nextElementSibling;
 
-			if(content.style.maxHeight) {
-				content.style.maxHeight = null;
-			} else {
-				content.style.maxHeight = content.scrollHeight + "px";
-			}
-		});
-	}
+		if(content.classList.contains("contentOpen")) {
+			content.classList.remove("contentOpen");
+		} else {
+			content.classList.add("contentOpen");
+		}
+		console.log(content.style.maxHeight);
+	});
+}
