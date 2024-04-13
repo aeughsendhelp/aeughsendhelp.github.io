350bd1777857a1859504269220f026a5904f1dde
EvilPunkin <82129788+EvilPunkin@users.noreply.github.com>
2024-04-12 20:17:51 -0700

    still need to fix shader issue

--- references/submarine/scene.js
+++ references/submarine/scene.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.114/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.114/examples/jsm/loaders/GLTFLoader.js';
 import { RGBELoader } from 'https://cdn.jsdelivr.net/npm/three@0.114/examples/jsm/loaders/RGBELoader.js';
-import { Water } from 'https://cdn.jsdelivr.net/npm/three@0.114/examples/jsm/objects/Water.js';
+import { Water } from './water.js';
 import { Sky } from 'https://cdn.jsdelivr.net/npm/three@0.114/examples/jsm/objects/Sky.js';
 import { clamp } from './utils.js';
 
@@ -11,7 +11,7 @@ const textureLoader = new THREE.TextureLoader();
 let water, sky, sun;
 
 let airFog;
-let airColor = new THREE.Color(0x90cae5);
+let airColor = new THREE.Color(0xdae2e5);
 let airFogDistance = 800;
 
 let waterFog;
@@ -19,10 +19,6 @@ let waterColor = new THREE.Color(0x071516);
 let waterFogDistance = 100;
 
 const skyParameters = {
-    turbidity: 10,
-    rayleigh: 3,
-    mieCoefficient: 0.005,
-    mieDirectionalG: 0.7,
     elevation: 20,
     azimuth: 180,
 };
@@ -66,7 +62,7 @@ export function initRenderer() {
 // ------------------------------------------
 
 export function fog(scene, cam) {
-    if(cam.position.y > 0.001) {
+    if(cam.position.y > 0.01) {
         scene.fog = airFog;
         scene.background = airColor;
     } else {
@@ -79,27 +75,37 @@ export function fog(scene, cam) {
 export function setupScene(scn) {
     scene = scn;
 
-    // renderer.toneMappingExposure = effectController.exposure;
-
-    const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x2b4c59, 5);
+    const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x2b4c59, 10);
     hemisphereLight.position.set(0, 10, 0);
     scene.add(hemisphereLight);
 
-    // const helper = new THREE.DirectionalLightHelper(sun);
-    // scene.add(helper);
 
-    // ground
-    sun = new THREE.Vector3();
 
-    const groundGeometry = new THREE.PlaneGeometry(1000, 1000);
+    const sun = new THREE.DirectionalLight(0xc7edfc, 5);
+    scene.add(sun);
+
+    const phi = THREE.MathUtils.degToRad( 90 - skyParameters.elevation );
+    const theta = THREE.MathUtils.degToRad( skyParameters.azimuth );
+    sun.position.setFromSphericalCoords( 1, phi, theta );
+
+    sun.target.position.set(0, 0, 0);
+    sun.target.updateMatrixWorld();
+
+    const helper = new THREE.DirectionalLightHelper(sun);
+    scene.add(helper);
+
+
+
+    const groundGeometry = new THREE.PlaneGeometry(5000, 5000);
     const groundMaterial = new THREE.MeshLambertMaterial({ color: 0xaaaaaa, side: THREE.DoubleSide });
     const ground = new THREE.Mesh(groundGeometry, groundMaterial);
     ground.rotation.x = Math.PI / 2;
     ground.position.y = -300;
     scene.add(ground);
 
+
     // ocean
-    const oceanGeometry = new THREE.PlaneGeometry(1000, 1000);
+    const oceanGeometry = new THREE.PlaneGeometry(5000, 5000);
     textureLoader.load('../references/submarine/waternormals.jpg',
         function(texture) {
             texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
@@ -115,62 +121,22 @@ export function setupScene(scn) {
                     waterColor: 0x001e0f,
                     distortionScale: 2,
                     fog: scene.fog !== undefined,
+                    side: THREE.DoubleSide,
                 }
             );
 
-
-            // const ocean = new THREE.Mesh(oceanGeometry, water);
             water.rotation.x = Math.PI / -2;
-            // ocean.position.y = 0;
             scene.add(water);
-
-            // Skybox
-            sky = new Sky();
-            sky.scale.setScalar( 10000 );
-            scene.add( sky );
-
-
-            // sun
-            const skyUniforms = sky.material.uniforms;
-            skyUniforms[ 'turbidity' ].value = skyParameters.turbidity;
-            skyUniforms[ 'rayleigh' ].value = skyParameters.rayleigh;
-            skyUniforms[ 'mieCoefficient' ].value = skyParameters.mieCoefficient;
-            skyUniforms[ 'mieDirectionalG' ].value = skyParameters.mieDirectionalG;
-
-            const phi = THREE.MathUtils.degToRad( 90 - skyParameters.elevation );
-            const theta = THREE.MathUtils.degToRad( skyParameters.azimuth );
-            sun.setFromSphericalCoords( 1, phi, theta );
-            const sunLight = new THREE.DirectionalLight(0xf2e7cb, 5);
-            scene.add(sunLight);
-
-            skyUniforms[ 'sunPosition' ].value.copy( sun );
-
-            sunLight.position.set(0, 10, 10);
-            sunLight.target.position.set(0, 0, 0);
-            sunLight.target.updateMatrixWorld();
 
             waterUniforms = water.material.uniforms;
             waterUniforms[ 'size' ].value = 6;
             waterUniforms[ 'sunDirection' ].value.copy( sun ).normalize();
-
-
-
-            // generating lighting DOESN'T FUCKING WORK, i think it's because the example i'm using uses an old version of three js
-            // if anyone knows how to make this work please tell me
-            /*
-            if ( renderTarget !== undefined ) renderTarget.dispose();
-
-            sceneEnv.add( sky );
-            renderTarget = pmremGenerator.fromScene( sceneEnv );
-
-            scene.add( sky );
-            scene.environment = renderTarget.texture;
-            */
         }
     );
+
     animate();
 }
-
+// should prob move this over to the animate function in main
 function animate() {
     requestAnimationFrame(animate);
 
